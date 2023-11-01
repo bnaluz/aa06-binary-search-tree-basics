6b43bc14e1d0259033845e1c7aa8dfa53a5174fd
Bobby Naluz <bnaluz9@gmail.com>
2023-10-31 20:14:21 -0400

    initial commit

---
+++ binary-search-tree.js
@@ -0,0 +1,77 @@
+// Do not change this
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val, currentNode = this.root) {
+    if (!this.root) {
+      this.root = new TreeNode(val);
+      return;
+    }
+
+    if (val > currentNode.val) {
+      if (!currentNode.right) {
+        currentNode.right = new TreeNode(val);
+      } else {
+        this.insert(val, currentNode.right);
+      }
+    } else {
+      if (val < currentNode.val) {
+        if (!currentNode.left) {
+          currentNode.left = new TreeNode(val);
+        } else {
+          this.insert(val, currentNode.left);
+        }
+      }
+    }
+  }
+
+  search(val, currentNode = this.root) {
+    if (this.root.val === val) {
+      return true;
+    }
+
+    if (!this.root) {
+      return false;
+    }
+
+    if (val < this.root.val) {
+      return this.search(val, currentNode.left);
+    } else {
+      return this.search(val, currentNode.right);
+    }
+  }
+
+  preOrderTraversal(currentNode = this.root) {
+    // Your code here
+  }
+
+  inOrderTraversal(currentNode = this.root) {
+    // Your code here
+  }
+
+  postOrderTraversal(currentNode = this.root) {
+    // Your code here
+  }
+
+  // Breadth First Traversal - Iterative
+  breadthFirstTraversal() {
+    // Your code here
+  }
+
+  // Depth First Traversal - Iterative
+  depthFirstTraversal() {
+    // Your code here
+  }
+}
+
+module.exports = { BinarySearchTree, TreeNode };
